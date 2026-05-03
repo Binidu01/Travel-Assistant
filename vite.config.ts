1b6d9bd62da59eecac911ade8f43e937e4ac8807
Binidu Ranasinghe <161937364+Binidu01@users.noreply.github.com>
2026-05-03 23:39:43 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ command, mode }) => {
     plugins: [
       tailwindcss(),
       react(),
-      biniroute({ platform: 'node' }),
+      biniroute({ platform: 'vercel' }),
       biniOverlay(),
       biniEnv(),
       biniExport(),
